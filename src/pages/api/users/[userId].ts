9cd288426f1a47328963635db36b87b3802efd78
patrik.bartok.013@gmail.com <patrik.bartok.013@gmail.com>
2023-10-03 13:07:17 +0200

    Refactored api

---
+++ src/pages/api/users/[userId].ts
@@ -0,0 +1,21 @@
+import { eq } from 'drizzle-orm'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '~/db'
+import { User, users } from '~/db/schema'
+
+export default async (req: NextApiRequest, res: NextApiResponse<User>) => {
+  const { userId } = req.query || 0
+
+  const userIdNum = Number(userId)
+
+  const user = await db.query.users.findFirst({
+    where: eq(users.id, userIdNum),
+    with: {
+      likedPosts: true
+    }
+  })
+  if (!user) {
+    throw new Error('Invalid user')
+  }
+  res.json(user)
+}
